11cdf30c4a82c5b476b817f81c781acc2a54a503
Nick Treleaven <n@trelsoft.com>
2019-04-30 17:12:32 +0100

    Fix map_close argument type and update test

--- closure.v
+++ closure.v
@@ -1,3 +1,4 @@
+// int to int version
 import intiter
 
 // Returns a closure
@@ -9,33 +10,27 @@ fn (it mut IntIter) map_close(f fn(int)int) fn()?int
     }
 }
 
-arr := [1,2,3]
-mut it := arr.iter();
-f := it.map_close(fn i {i * i})
-// print 149
-for e := f()
+fn test_map()
 {
-    print(e)
+    arr := [1,2,3]
+    mut it := arr.iter();
+    f := it.map_close(fn i {i * i})
+    // print 149
+    for
+    {
+        e := f() or {break}
+        print(e)
+    }
 }
 
 //// Generic version
 import iter
 
 // Returns a closure
-fn (it mut It<T!>) map_close(f fn(T)R!) fn()?R
+fn (it mut It!) map_close(f fn(IterElement<It>)R!) fn()?R
 {
     return fn {
         e := it.next()?
         return f(e)
     }
 }
-
-arr := [1,2,3]
-mut it := arr.iter();
-f := it.map_close(fn i {i * i})
-// print 149
-for e := f()
-{
-    print(e)
-}
-
