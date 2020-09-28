da76d61c880bdb5ab2dfd5cd814fb66f567b782a
Nick Treleaven <n@trelsoft.com>
2020-09-28 13:27:12 +0100

    fixes + test cap()

--- vlib/array.v
+++ vlib/array.v
@@ -11,36 +11,52 @@ pub:
 // allocated on the heap
 struct ArrayAlloc
 {
+mut:
 	cap  int
 	data [0]byte // variable size
 }
 
-[inline]
-fn __new_array_alloc(cap, elem_size int) voidptr {
-	ptr := malloc(sizeof(int) + cap * elem_size)
-	aa := &ArrayAlloc(ptr)
+[inline] [unsafe]
+fn new_array_alloc(cap, elem_size int) voidptr {
+	ptr := malloc(int(sizeof(int)) + cap * elem_size)
+	mut aa := &ArrayAlloc(ptr)
 	aa.cap = cap
-	return &aa.data
+	return voidptr(&aa.data[0])
 }
 
-fn __new_array(len, cap, elem_size int) Array {
-	cap_ := if cap < mylen { mylen } else { cap }
-	alloc = __new_array_alloc(cap_, elem_size)
-	C.memset(alloc, 0, len * elem_size)
+fn new_array(len, cap, elem_size int) Array {
+	cap_ := if cap < len { len } else { cap }
+	data := new_array_alloc(cap_, elem_size)
+	unsafe {C.memset(data, 0, len * elem_size)}
 	arr := Array{
 		element_size: elem_size
-		data: &alloc.data
+		data: data
 		len: len
-		start: &alloc.data
+		start: data
 	}
 	return arr
 }
 
 [inline]
-fn (a Array) cap() {
+fn (a Array) cap() int {
 	if a.start != a.data {
 		return a.len
 	}
-	alloc := &ArrayAlloc(a.start - sizeof(ArrayAlloc))
+	alloc := &ArrayAlloc(unsafe {a.start - sizeof(ArrayAlloc)})
 	return alloc.cap
+}
+
+fn main() {
+	a := new_array(2, 4, 1)
+	//~ println(a.set_unsafe(0, 10))
+	//~ assert a.get_unsafe(0) == 10
+	//~ assert a.get_unsafe(1) == 0
+	//~ println(a.get_unsafe(1))
+	println(&byte(a.data)[0])
+	println(&byte(a.data)[3])
+	println(a.cap())
+	assert a.cap() == &int(a.data)[-1]
+	
+	b := new_array(2, 0, 1)
+	assert b.cap() == 2
 }
